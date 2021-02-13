dae37364c740c53a7fea14cfd7f92c3d32d34286
Pepivsky <ironglas68@gmail.com>
2021-02-13 15:07:20 -0600

    Add splashSreen with light/night theme

--- app/src/main/java/com/hacktzi/blog/MainActivity.kt
+++ app/src/main/java/com/hacktzi/blog/MainActivity.kt
@@ -5,8 +5,7 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        //Thread.sleep(10000)//show splashScreen 10 seconds
-        setTheme(R.style.Theme_HackTziBlog) //set normal theme before show activity
+        setTheme(R.style.Theme_HackTziBlog)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
